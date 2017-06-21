4f2aee0c09388c4043cf37bb2581f9c304af0ced
carolynemilgo <carolynemilgo@gmail.com>
2017-06-21 10:19:02 +0300

    updated hold function and setting of high score

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # A game of PIG DICE
 ## Author
 Carolyne Milgo
-# Description
+## Description
 The game allows two users to play against each other by rolling DICE
-# Specifications
+## Specifications
 1. The first player rolls the dice and it returns a number between 1 and 6.
 Input: 1
 Output: The next player should roll the dice
@@ -19,7 +19,12 @@ Output: Continue rolling the dice.
 acquired and the counter resets to one
 input: 1
 Output: new count is 0
-# Developed using
+5. The players can hold the game at any point using the hold button and their points remain the same as they increment when they resume their turn to roll the dice.
+6. The high score of 50 is set to define the winner of the game and in the event that the cumulative score for one of the players is equal to or greater than 50, that winner is declared the winner.
+input: score>=50
+Output: You are the winner
+## Developed using
 The game of pig dice is developed using HTML, CSS and Javascript
-# Link
-The game is accesible on github through this link
+## Link
+The game is accesible on github through this link:
+https://carolynemilgo.github.io/pigdice/
--- js/script.js
+++ js/script.js
@@ -1,71 +1,76 @@
-
-function playerA(randomA){
-  this.randomA=randomA;
+//Business Logic
+function playerA(randomA) {
+  this.randomA = randomA;
 }
-function playerB(randomB){
-  this.randomB=randomB;
+function playerB(randomB) {
+  this.randomB = randomB;
 }
-//prototypes
-playerA.prototype.scoreA=function(){
-  return this.randomA;
+playerA.prototype.holdOne = function() {
+  $("button#buttonA").prop('disabled', true);
+  $("button#buttonB").prop('disabled', false);
+  $("button#holdA").prop('disabled', true);
+  $("button#holdB").prop('disabled', false);
+  alert("You have hold your cards");
 }
-playerB.prototype.scoreB=function(){
-  return this.randomB;
+playerB.prototype.holdTwo = function() {
+  $("button#buttonA").prop('disabled', false);
+  $("button#buttonB").prop('disabled', true);
+  $("button#holdB").prop('disabled', true);
+  $("button#holdA").prop('disabled', false);
+  alert("You have hold your cards");
 }
+//User Interface LOgic
 //playerA
-$(document).ready(function(){
-  //var scorePlayerA=[];
-  var newPlayerA=new playerA(playerOne);
-  var playerOne=0;
-  var newPlayerB=new playerB(playerTwo);
-  var playerTwo=0;
-  //var totalForA=scoreplayerA.reduce(function (a, playerOne){return a + playerOne;},0);
-  $("form#playerA").submit(function(event){
+$(document).ready(function() {
+  var newPlayerA = new playerA(playerOne);
+  var playerOne = 0;
+  var newPlayerB = new playerB(playerTwo);
+  var playerTwo = 0;
+  $("form#playerA").submit(function(event) {
     event.preventDefault();
-    var randomNumberA=Math.floor(Math.random() * 6) + 1;
-    if(randomNumberA===1){
+    var randomNumberA = Math.floor(Math.random() * 6) + 1;
+    if (randomNumberA === 1) {
       alert("Hahaaaa...You have burnt!!!!");
-      $("button#buttonA").prop('disabled',true);
-      $("button#buttonB").prop('disabled',false);
-      //playerOne=0;
-    }
-    else{
-        playerOne+=randomNumberA;
-      }
-    //  console.log(playerOne);
-      $("h4#playerOne").text(playerOne);
-      //$("#playerOne").text(newPlayerA.scoreA());
-    //  $("h4#playerOne").text("<span class='resultA'>" + playerOne() + "</span>");
-  });
-  $("form#playerB").submit(function(event){
-    event.preventDefault();
-    var randomNumberB=Math.floor(Math.random() * 6) + 1;
-    if(randomNumberB===1){
-      alert("Hahaaaa...You have burnt!!");
-      $("button#buttonB").prop('disabled',true);
-      $("button#buttonA").prop('disabled',false);
-      //playerTwo=0;
-    }
-    else{
-      playerTwo+=randomNumberB;
-    }
+      $("button#buttonA").prop('disabled', true);
+      $("button#buttonB").prop('disabled', false);
 
-//     if($("button#buttonA").prop('disabled',true)){
-//       $("button#buttonB").prop('disabled',false);
-//     }
-//     else ($("button#buttonB").prop('disabled',true)){
-//       $("button#buttonA").prop('disabled',false);
-// }
-$("h4#playerTwo").text(playerTwo);
+      playerOne = 0;
+      $("button#holdB").prop('disabled', false);
+      $("button#buttonB").prop('disabled', false);
+    } else {
+      playerOne += randomNumberA;
+    }
+    //winner and score set to 20
+    if (playerOne >= 20) {
+      alert("You are the winner Player1");
+    }
+    $("h4#playerOne").text(playerOne);
+  });
+  $("form#hold").submit(function(event) {
+    event.preventDefault();
+    newPlayerA.holdOne();
+  });
+  //Player Two
+  $("form#playerB").submit(function(event) {
+    event.preventDefault();
+    var randomNumberB = Math.floor(Math.random() * 6) + 1;
+    if (randomNumberB === 1) {
+      alert("Hahaaaa...You have burnt!!");
+      $("button#buttonB").prop('disabled', true);
+      $("button#buttonA").prop('disabled', false);
+      playerTwo = 0;
+    } else {
+      playerTwo += randomNumberB;
+    }
+      //winner and score set to 20
+    if (playerTwo >= 20) {
+      alert("You are the winner Player2");
+    }
+    $("h4#playerTwo").text(playerTwo);
     console.log(playerTwo);
-    //$("#playerTwo").text(newPlayerB.scoreB());
+  });
+  $("form#hold2").submit(function(event) {
+    event.preventDefault();
+    newPlayerB.holdTwo();
   });
 });
-//playerB
-/*
-  var newPlayerB=new playerB(playerTwo);
-  var PlayerTwo=0;
-*/
-if($("button#holdA").prop('disabled', true)){
-  $("button#buttonA").prop('disabled',false);
-}
